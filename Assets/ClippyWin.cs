7ef1b212bf00d46412fb97feb4402dfd1edad3ae
salt411 <137428508+salt411@users.noreply.github.com>
2024-01-27 16:49:46 -0500

    first commit of revertrevert
    includes everything from like... 4am.

--- Assets/ClippyWin.cs
+++ Assets/ClippyWin.cs
@@ -4,12 +4,14 @@ using UnityEngine;
 
 public class ClippyWin : MonoBehaviour
 {
+    public bool hasWon = false;
     private void OnTriggerEnter(Collider other)
     {
         var clippy = other.GetComponent<Clippy>();
-        if(clippy != null )
+        if (clippy != null)
         {
             Debug.Log("Lose!");
+            hasWon = true;
         }
     }
 }
